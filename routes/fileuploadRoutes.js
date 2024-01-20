dae4c6309d9ca810ac3787fd34e07315b4569416
shivamBrihat <shivam@brihatinfotech.com>
2024-01-20 20:44:05 +0530

    Versioning untracked files

--- routes/fileuploadRoutes.js
+++ routes/fileuploadRoutes.js
@@ -1,6 +1,14 @@
 const cloudinary = require('cloudinary').v2;
 const router = require("express").Router();
 
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECREAT,
+  });
+
 router.post("/single",async (req,res)=>{
     const file = req.files.file
     console.log({file})
