e81ccbe8ae78e7ca9e20b131f8b1602633be4262
shivamBrihat <shivam@brihatinfotech.com>
2024-01-23 18:42:27 +0530

    Versioning untracked files

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -8,6 +8,6 @@ router.post('/comments/:id/like', commentController.likeComment);
 router.post('/comments/:id/last-interaction', commentController.getLastInteraction);
 router.post('/comments/:id/dislike', commentController.dislikeComment);
 router.post('/comments/:id/report', commentController.reportComment);
-router.post('/blogs/:id/comments', commentController.getCommentsForBlog);
+router.post('/blogs/:id/comments/all', commentController.getCommentsForBlog);
 
 module.exports = router;
