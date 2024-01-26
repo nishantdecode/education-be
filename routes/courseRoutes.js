f39182f1fea761fb6f1d99965f421be4082b33f9
shivamBrihat <shivam@brihatinfotech.com>
2024-01-26 19:54:14 +0530

    Versioning untracked files

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,13 +1,11 @@
 // routes/courseRoutes.js
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const upload = multer();
 const courseController = require('../controllers/courseController');
 const { singleFileUploadMiddleware } = require('../middlewares/file-upload.middlewares');
 
 router.post('/', courseController.create);
-router.post('/import',upload.single('file'), courseController.importCourseData);
+router.post('/import',singleFileUploadMiddleware('file'), courseController.importCourseData);
 router.get('/', courseController.getAllCourses);
 router.get('/:id', courseController.getCourseById);
 router.put('/:id', courseController.updateCourse);
