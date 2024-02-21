f72c30e67b5aa376027b32535e2eec56d5f72a6c
shivamBrihat <shivam@brihatinfotech.com>
2024-02-21 20:00:10 +0530

    first commit

--- controllers/coachingController.js
+++ controllers/coachingController.js
@@ -1,101 +1,117 @@
-const { Coaching, createCoaching } = require('../models/coaching');
+const { Coaching, createCoaching } = require("../models/coaching");
 
 // Create a coaching
 const create = async (req, res) => {
-    try {
-        const coaching = await createCoaching(req.body);
-        res.status(201).json({ message: 'Coaching created successfully', coaching });
-    } catch (error) {
-        res.status(500).json({ message: 'Error creating coaching', error: error.message });
-    }
+  try {
+    const coaching = await createCoaching(req.body);
+    res
+      .status(201)
+      .json({ message: "Coaching created successfully", coaching });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating coaching", error: error.message });
+  }
 };
 
 // Get all coachings
 const getAllCoachings = async (req, res) => {
-    try {
-        let { page = 0, show = 10 } = req.query;
-        if (page) {
-          page = parseInt(page);
-        }
-        if (show) {
-          show = parseInt(show);
-        }
-        let coachings;
-        if (page && show) {
-            coachings = await Coaching.findAll({
-                offset: page * show,
-                limit: show,
-            });
-        } else {
-            coachings = await Coaching.findAll();
-                }
-        const totalCount = Coaching.findAll();
-        res.status(200).json({ 
-            message: 'All coachings retrieved successfully', 
-            coachings, 
-            currentPage: page,
-            totalCount: totalCount.length()
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving coachings', error: error.message });
+  try {
+    let { page = 0, show = 10 } = req.query;
+    if (page) {
+      page = parseInt(page);
     }
+    if (show) {
+      show = parseInt(show);
+    }
+    let coachings;
+    if (page && show) {
+      coachings = await Coaching.findAll({
+        offset: page * show,
+        limit: show,
+      });
+    } else {
+      coachings = await Coaching.findAll();
+    }
+    const totalCount = await Coaching.count();
+    res.status(200).json({
+      message: "All coachings retrieved successfully",
+      coachings,
+      currentPage: page,
+      totalCount: totalCount,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving coachings", error: error.message });
+  }
 };
 
 // Get a coaching by ID
 const getCoachingById = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const coaching = await Coaching.findByPk(id);
-        if (!coaching) {
-            return res.status(404).json({ message: 'Coaching not found' });
-        }
-        res.status(200).json({ message: 'Coaching retrieved successfully', coaching });
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving coaching', error: error.message });
+  try {
+    const coaching = await Coaching.findByPk(id);
+    if (!coaching) {
+      return res.status(404).json({ message: "Coaching not found" });
     }
+    res
+      .status(200)
+      .json({ message: "Coaching retrieved successfully", coaching });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving coaching", error: error.message });
+  }
 };
 
 // Update a coaching by ID
 const updateCoaching = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const coaching = await Coaching.findByPk(id);
-        if (!coaching) {
-            return res.status(404).json({ message: 'Coaching not found' });
-        }
-
-        await coaching.update({ data: req.body });
-
-        res.status(200).json({ message: 'Coaching updated successfully', coaching });
-    } catch (error) {
-        res.status(500).json({ message: 'Error updating coaching', error: error.message });
+  try {
+    const coaching = await Coaching.findByPk(id);
+    if (!coaching) {
+      return res.status(404).json({ message: "Coaching not found" });
     }
+
+    await coaching.update({ data: req.body });
+
+    res
+      .status(200)
+      .json({ message: "Coaching updated successfully", coaching });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating coaching", error: error.message });
+  }
 };
 
 // Delete a coaching by ID
 const deleteCoaching = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const coaching = await Coaching.findByPk(id);
-        if (!coaching) {
-            return res.status(404).json({ message: 'Coaching not found' });
-        }
-
-        await coaching.destroy();
-
-        res.status(200).json({ message: 'Coaching deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error deleting coaching', error: error.message });
+  try {
+    const coaching = await Coaching.findByPk(id);
+    if (!coaching) {
+      return res.status(404).json({ message: "Coaching not found" });
     }
+
+    await coaching.destroy();
+
+    res.status(200).json({ message: "Coaching deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting coaching", error: error.message });
+  }
 };
 
 module.exports = {
-    create,
-    getAllCoachings,
-    getCoachingById,
-    updateCoaching,
-    deleteCoaching,
+  create,
+  getAllCoachings,
+  getCoachingById,
+  updateCoaching,
+  deleteCoaching,
 };
