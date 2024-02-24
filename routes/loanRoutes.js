0820e5e4f00b72f614e35dc11b224349e1bc77b4
shivamBrihat <shivam@brihatinfotech.com>
2024-02-24 12:07:57 +0530

    first commit

--- routes/loanRoutes.js
+++ routes/loanRoutes.js
@@ -6,7 +6,7 @@ const loanController = require('../controllers/loanController');
 router.post('/', loanController.create);
 
 // Get all loans
-router.post('/', loanController.getAllLoans);
+router.post('/all', loanController.getAllLoans);
 
 // Get a loan by ID
 router.get('/:id', loanController.getLoanById);
