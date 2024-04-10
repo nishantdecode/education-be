ae091a0e882dcea3f85842cdf0226d421513776e
shivamBrihat <shivam@brihatinfotech.com>
2024-04-10 14:08:23 +0530

    Search params issue resolved '

--- routes/fileuploadRoutes.js
+++ routes/fileuploadRoutes.js
@@ -14,18 +14,23 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('file');
 const { Readable } = require('stream');
 
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+}
 router.post("/single", upload, async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
-    const stream = Readable.from(req.file.buffer);
-
-    // Upload file to Cloudinary
-    const result = await cloudinary.uploader.upload(stream, { resource_type: 'raw' });
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
   
     // Send the file URL in the response
-    res.json({ fileUrl: result.url });
+    res.json({ fileUrl: cldRes });
   } catch (error) {
     console.error('Error uploading file to Cloudinary:', error);
     res.status(500).json({ message: 'Error uploading file' });
