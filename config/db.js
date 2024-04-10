10fe916e0cd71d7cd20b3edb7b5dd265a924ece1
shivamBrihat <shivam@brihatinfotech.com>
2024-04-10 13:00:27 +0530

    Search params issue resolved '

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const databaseUrl = "postgresql://postgres:007@154.49.243.15:5432/edmertion";
+const databaseUrl = "postgresql://postgres:9910@localhost:5432/edmertion";
 
 const match = databaseUrl.match(/^postgresql:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)$/);
 if (!match) {
   throw new Error('DATABASE_URL is not correctly formatted');
 }
 
-const [ username, password, host, port, database] = match;
+const [, username, password, host, port, database] = match;
 
 const sequelize = new Sequelize(database, username, password, {
   host,
